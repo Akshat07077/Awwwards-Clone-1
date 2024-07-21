167d55cac3b4e9f40c7cb6608a434c460bf9d903
Akshat Sharma <sakshat988@gmail.com>
2024-07-22 00:27:35 +0530

    locomotive update

--- app/page.js
+++ app/page.js
@@ -9,6 +9,22 @@ import Featured from './Components/Featured'
 import Cards from './Components/Cards'
 import LocomotiveScroll from 'locomotive-scroll';
 
+export const locomotiveScroll = new LocomotiveScroll({
+  lenisOptions: {
+	  wrapper: window,
+	  content: document.documentElement,
+	  lerp: 0.1,
+	  duration: 0.6,
+	  orientation: 'vertical',
+	  gestureOrientation: 'vertical',
+	  smoothWheel: true,
+	  smoothTouch: true,
+	  wheelMultiplier: 1,
+	  touchMultiplier: 0.5,
+	  normalizeWheel: true,
+	  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+  }
+});
 const Page = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
