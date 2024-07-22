fc23a2063fffa60ab8005e28ba3682c2bc7760ec
Akshat Sharma <sakshat988@gmail.com>
2024-07-22 14:37:37 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,48 +1,47 @@
 "use client"
-import React, { useEffect } from 'react'
-import Navbar from './Components/Navbar'
-import Landing from './Components/Landing'
-import { Marque } from './Components/Marque'
-import About from './Components/About'
-import { Eyes } from './Components/Eyes'
-import Featured from './Components/Featured'
-import Cards from './Components/Cards'
-import LocomotiveScroll from 'locomotive-scroll';
+import dynamic from 'next/dynamic';
+import React, { useEffect } from 'react';
+import Navbar from './Components/Navbar';
+import { Marque } from './Components/Marque';
+import About from './Components/About';
+import Featured from './Components/Featured';
+import Landing from './Components/Landing';
+import Cards from './Components/Cards';
+const Eyes = dynamic(() => import('./Components/Eyes'), { ssr: false });
 
-export const locomotiveScroll = new LocomotiveScroll({
-  lenisOptions: {
-	  wrapper: window,
-	  content: document.documentElement,
-	  lerp: 0.1,
-	  duration: 0.6,
-	  orientation: 'vertical',
-	  gestureOrientation: 'vertical',
-	  smoothWheel: true,
-	  smoothTouch: true,
-	  wheelMultiplier: 1,
-	  touchMultiplier: 0.5,
-	  normalizeWheel: true,
-	  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-  }
-});
-const Page = () => {
+
+const HomePage = () => {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const locomotiveScroll = new LocomotiveScroll();
-    }
+    // Import locomotive-scroll dynamically on the client side
+    const initializeLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+      });
+
+      // Clean up
+      return () => {
+        scroll.destroy();
+      };
+    };
+
+    const cleanup = initializeLocomotiveScroll();
+    return () => cleanup.then(clean => clean());
   }, []);
 
   return (
     <div className='w-full min-h-screen bg-zinc-800'>
-      <Navbar />
-      <Landing />
-      <Marque />
-      <About />
+      <Navbar/>
+      <Landing/>
+      <Marque/>
+      <About/>
       <Eyes />
-      <Featured />
-      <Cards />
+      <Featured/>
+      <Cards/>
+      {/* Other components */}
     </div>
   );
-}
+};
 
-export default Page;
+export default HomePage;
